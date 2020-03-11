3ef3a884c562743e4b3a7ec62f7335390ac53e34
Luis Leopardi <supernano1998@gmail.com>
2020-03-11 05:58:04 -0400

    Updates

---
+++ precache-manifest.40cadcf10a9cbc2f1b7a9c280183f9ca.js
@@ -0,0 +1,290 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0680eb2f4b5c5ad216e453b48e8bb9",
+    "url": "/newsApi/index.html"
+  },
+  {
+    "revision": "e00220e5f1c486267f38",
+    "url": "/newsApi/static/css/main.191f87cd.chunk.css"
+  },
+  {
+    "revision": "60e3874954557fecd5d7",
+    "url": "/newsApi/static/js/2.bcf724ea.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/newsApi/static/js/2.bcf724ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e00220e5f1c486267f38",
+    "url": "/newsApi/static/js/main.876735fd.chunk.js"
+  },
+  {
+    "revision": "79feace19cac3df5ba9c",
+    "url": "/newsApi/static/js/runtime-main.409370a7.js"
+  },
+  {
+    "revision": "83803dbbed17bc537cb233a08cdc75bc",
+    "url": "/newsApi/static/media/ae.83803dbb.svg"
+  },
+  {
+    "revision": "02c6b226e853d2fe23aff40fd1cca79b",
+    "url": "/newsApi/static/media/ar.02c6b226.svg"
+  },
+  {
+    "revision": "fef04a56666d4fd4f18c70cc656d77dd",
+    "url": "/newsApi/static/media/at.fef04a56.svg"
+  },
+  {
+    "revision": "842ef7577e70afaaf2466eaac6ab001a",
+    "url": "/newsApi/static/media/au.842ef757.svg"
+  },
+  {
+    "revision": "38a9aea5f2621af487117a077f026678",
+    "url": "/newsApi/static/media/be.38a9aea5.svg"
+  },
+  {
+    "revision": "0f0e045b34ad70f86ffddd7cb79fd5a7",
+    "url": "/newsApi/static/media/bg.0f0e045b.svg"
+  },
+  {
+    "revision": "a2bd0d7b2b229a31265df45db55939e7",
+    "url": "/newsApi/static/media/br.a2bd0d7b.svg"
+  },
+  {
+    "revision": "f22bc5ac0f3c21fa7c9f8c8d89559595",
+    "url": "/newsApi/static/media/business.f22bc5ac.svg"
+  },
+  {
+    "revision": "518fb22bf46e9b367d576752ea1c52a7",
+    "url": "/newsApi/static/media/ca.518fb22b.svg"
+  },
+  {
+    "revision": "9c139812f541902e3c71c740096b5b45",
+    "url": "/newsApi/static/media/ch.9c139812.svg"
+  },
+  {
+    "revision": "35e45f0ecfe9dd0fb06ada43f7dc7c0e",
+    "url": "/newsApi/static/media/cn.35e45f0e.svg"
+  },
+  {
+    "revision": "19370811f34797d5ae1f4a07bd421298",
+    "url": "/newsApi/static/media/co.19370811.svg"
+  },
+  {
+    "revision": "d0e30a318b027405147ad765eb4a5cf7",
+    "url": "/newsApi/static/media/compass.d0e30a31.svg"
+  },
+  {
+    "revision": "9e717ab8b1976df5539407af99b10976",
+    "url": "/newsApi/static/media/cu.9e717ab8.svg"
+  },
+  {
+    "revision": "83d3c9a543fac2ea5390bb59ceb9b85a",
+    "url": "/newsApi/static/media/cz.83d3c9a5.svg"
+  },
+  {
+    "revision": "69e304cd3b0332801a4788976c770911",
+    "url": "/newsApi/static/media/de.69e304cd.svg"
+  },
+  {
+    "revision": "5d78c34fcd0f4ad1a59a951ad4400562",
+    "url": "/newsApi/static/media/eg.5d78c34f.svg"
+  },
+  {
+    "revision": "4e96695c42c2859b6d0ad051705aac70",
+    "url": "/newsApi/static/media/entertainment.4e96695c.svg"
+  },
+  {
+    "revision": "524f308751047f8626f60e9edaadaf29",
+    "url": "/newsApi/static/media/flag.524f3087.svg"
+  },
+  {
+    "revision": "78126112018bb65f41ea59eb23acae9a",
+    "url": "/newsApi/static/media/fr.78126112.svg"
+  },
+  {
+    "revision": "386cc387066317cf209c9abf7e98afab",
+    "url": "/newsApi/static/media/gb.386cc387.svg"
+  },
+  {
+    "revision": "3b7f694b8cfeb4c43ffda6427a7a52f7",
+    "url": "/newsApi/static/media/general.3b7f694b.svg"
+  },
+  {
+    "revision": "095ffbf1beff139064743fab09ce9d4b",
+    "url": "/newsApi/static/media/gr.095ffbf1.svg"
+  },
+  {
+    "revision": "622bba7a793b5525499e709a3da9b55d",
+    "url": "/newsApi/static/media/health.622bba7a.svg"
+  },
+  {
+    "revision": "ebfb3038960c4496fb61596cbe407212",
+    "url": "/newsApi/static/media/hk.ebfb3038.svg"
+  },
+  {
+    "revision": "7ea0b9803573c5017d5e2a53672f0f73",
+    "url": "/newsApi/static/media/hu.7ea0b980.svg"
+  },
+  {
+    "revision": "9849793de50fb86ca399b0dd7ff29ce0",
+    "url": "/newsApi/static/media/id.9849793d.svg"
+  },
+  {
+    "revision": "e4efda8ea5094189dbd4a3465c2dc998",
+    "url": "/newsApi/static/media/ie.e4efda8e.svg"
+  },
+  {
+    "revision": "36a29225c0c51c020f0ada5e4be130c1",
+    "url": "/newsApi/static/media/il.36a29225.svg"
+  },
+  {
+    "revision": "22ed30bb1b8910eeff0b7fac5d40e07e",
+    "url": "/newsApi/static/media/in.22ed30bb.svg"
+  },
+  {
+    "revision": "28e4004ddcf47908cd69954f9396e123",
+    "url": "/newsApi/static/media/info.28e4004d.svg"
+  },
+  {
+    "revision": "15dd512f4ac2ead07046ca7a3dc32c33",
+    "url": "/newsApi/static/media/it.15dd512f.svg"
+  },
+  {
+    "revision": "180bed75a431a4acc7d7860a93f14aac",
+    "url": "/newsApi/static/media/jp.180bed75.svg"
+  },
+  {
+    "revision": "5bd50db07092cd7116ea7e04aeaccfe2",
+    "url": "/newsApi/static/media/kr.5bd50db0.svg"
+  },
+  {
+    "revision": "73d0609a4b976a0015b47ace4fac1e8e",
+    "url": "/newsApi/static/media/lens.73d0609a.svg"
+  },
+  {
+    "revision": "9e52912be088076541d780de045d81ec",
+    "url": "/newsApi/static/media/lt.9e52912b.svg"
+  },
+  {
+    "revision": "4e95562670914e1cbb0c269e6e9ac64f",
+    "url": "/newsApi/static/media/lv.4e955626.svg"
+  },
+  {
+    "revision": "239a227a4bf69fd17b25fc616521d65e",
+    "url": "/newsApi/static/media/ma.239a227a.svg"
+  },
+  {
+    "revision": "fab19f7ffe594aaee45541de5e3a93a5",
+    "url": "/newsApi/static/media/mx.fab19f7f.svg"
+  },
+  {
+    "revision": "8cd3543b7a6e28923f855e8838f14049",
+    "url": "/newsApi/static/media/my.8cd3543b.svg"
+  },
+  {
+    "revision": "db6ea0cbd2d7df26b2a5f436d735306f",
+    "url": "/newsApi/static/media/ng.db6ea0cb.svg"
+  },
+  {
+    "revision": "3176ecb22e25260916af0f0c71e01524",
+    "url": "/newsApi/static/media/nl.3176ecb2.svg"
+  },
+  {
+    "revision": "ddb3247b95566d859a5cc10ddeb15b26",
+    "url": "/newsApi/static/media/no.ddb3247b.svg"
+  },
+  {
+    "revision": "e0d0171535d503933375630f9e7c9c2f",
+    "url": "/newsApi/static/media/nz.e0d01715.svg"
+  },
+  {
+    "revision": "65f451d126a62fb6f8a1599f6be8d42c",
+    "url": "/newsApi/static/media/ph.65f451d1.svg"
+  },
+  {
+    "revision": "bd4c370a8b4522518f666b2b6b6b3375",
+    "url": "/newsApi/static/media/pl.bd4c370a.svg"
+  },
+  {
+    "revision": "d6d5e4d917e85b91f8cb835bd1864395",
+    "url": "/newsApi/static/media/pt.d6d5e4d9.svg"
+  },
+  {
+    "revision": "c3ba82a536fe4983dad4c06b578dcd5d",
+    "url": "/newsApi/static/media/reactLogo.c3ba82a5.png"
+  },
+  {
+    "revision": "a9894617db0b9383b94cdcc4076d9713",
+    "url": "/newsApi/static/media/ro.a9894617.svg"
+  },
+  {
+    "revision": "f2ac985bf18aa3c586be85974e92a3b9",
+    "url": "/newsApi/static/media/rs.f2ac985b.svg"
+  },
+  {
+    "revision": "2434e4f0658fa694dc7c578e21950193",
+    "url": "/newsApi/static/media/ru.2434e4f0.svg"
+  },
+  {
+    "revision": "30b5c287229bd6887ad20967f35c94b4",
+    "url": "/newsApi/static/media/sa.30b5c287.svg"
+  },
+  {
+    "revision": "763dfbb9675b0a2cfefda44f19d197cf",
+    "url": "/newsApi/static/media/science.763dfbb9.svg"
+  },
+  {
+    "revision": "a5556539de7cc60844382ef42cd8dfec",
+    "url": "/newsApi/static/media/se.a5556539.svg"
+  },
+  {
+    "revision": "c04c9d0a764ad6c2027883b9caefdec4",
+    "url": "/newsApi/static/media/sg.c04c9d0a.svg"
+  },
+  {
+    "revision": "d0e350e1a9ccdaa65df0198178b1a224",
+    "url": "/newsApi/static/media/si.d0e350e1.svg"
+  },
+  {
+    "revision": "f6d2f33ec5a49d28cc11cc148f9fc3d4",
+    "url": "/newsApi/static/media/sk.f6d2f33e.svg"
+  },
+  {
+    "revision": "2fb274c762cd1e162383e76f10e542e7",
+    "url": "/newsApi/static/media/sports.2fb274c7.svg"
+  },
+  {
+    "revision": "a76fe883a256a6d1e98ebe96a7ede4a0",
+    "url": "/newsApi/static/media/technology.a76fe883.svg"
+  },
+  {
+    "revision": "d2464b11eb2cdbc7832f45b540c7b42b",
+    "url": "/newsApi/static/media/th.d2464b11.svg"
+  },
+  {
+    "revision": "0b7944f2a2a746a2a2e95ffa665df307",
+    "url": "/newsApi/static/media/tr.0b7944f2.svg"
+  },
+  {
+    "revision": "177c621be645ca8ef93610ef8e2dead5",
+    "url": "/newsApi/static/media/tw.177c621b.svg"
+  },
+  {
+    "revision": "2fed2ba2fc946fe1034d47012249028b",
+    "url": "/newsApi/static/media/ua.2fed2ba2.svg"
+  },
+  {
+    "revision": "a035edadeadb79c859f4a31c4392b1bb",
+    "url": "/newsApi/static/media/us.a035edad.svg"
+  },
+  {
+    "revision": "c63e49d473b01c53484a93c58ab83b29",
+    "url": "/newsApi/static/media/ve.c63e49d4.svg"
+  },
+  {
+    "revision": "0c1b162d57e16847dac513abc9b64637",
+    "url": "/newsApi/static/media/za.0c1b162d.svg"
+  }
+]);
